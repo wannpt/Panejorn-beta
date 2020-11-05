27fa5e843f9f0707b8a40f71c644696575491dd6
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2020-11-05 15:37:36 +0700

    Set CI

---
+++ frontend/src/components/Navbar/Navbar.spec.tsx
@@ -0,0 +1,11 @@
+import { screen, render } from "@testing-library/react"
+import React from "react"
+import Navbar from "./Navbar"
+
+describe('Navbar test',() => {
+    it('Should be rendered', ()=> {
+        const test_elem = (<Navbar />)
+        render(test_elem);
+        expect(screen.getByText('Plan collections')).toBeInTheDocument();
+    })
+}) 
