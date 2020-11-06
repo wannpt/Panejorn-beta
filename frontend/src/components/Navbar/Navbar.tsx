7c70e13563f703904c80fe4cb9ef790f84b30d8f
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2020-11-06 11:42:40 +0700

    Add navbar component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,17 +3,24 @@ import './Navbar.css';
 
 import { Icon } from '@iconify/react';
 import home4Fill from '@iconify/icons-ri/home-4-fill';
+import outlineCollectionsBookmark from '@iconify/icons-ic/outline-collections-bookmark';
+import compass3line from '@iconify/icons-ri/compass-3-line';
+import user3line from '@iconify/icons-ri/user-3-line';
 
 function Navbar() {
   return (
-    <div className="navbar">
-        <div className='row'>
-            <div className='col'>
-                <Icon icon={home4Fill} style={{color: '#E66973', fontSize: '20px'}} />
-            </div>
-            <div className='col'>
-                <p> Plan collections</p>
-            </div>
+    <div className="navbar text-center ">
+        <div className='col-3'>
+            <Icon icon={home4Fill} style={{color: '#E66973', fontSize: '20px'}} />
+        </div>
+        <div className='col-3'>
+            <Icon icon={outlineCollectionsBookmark} style={{color: '#E66973', fontSize: '20px'}} />
+        </div>
+        <div className='col-3'>
+            <Icon icon={compass3line} style={{color: '#E66973', fontSize: '20px'}} />
+        </div>
+        <div className='col-3'>
+            <Icon icon={user3line} style={{color: '#E66973', fontSize: '20px'}} />
         </div>
     </div>
   );
