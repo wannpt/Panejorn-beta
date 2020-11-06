14125837978d5e564d3f4a79afd11ea572acdb5f
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2020-11-06 13:34:27 +0700

    Continue add routing system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import RootRoutes from './routing/RootRouting';
+
 
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <RootRoutes />
       <Navbar />
     </div>
   );
--- frontend/src/routing/RootRouting.tsx
+++ frontend/src/routing/RootRouting.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Loadable from 'react-loadable';
 
 const Homepage = Loadable({
@@ -6,8 +7,20 @@ const Homepage = Loadable({
     loading: () => null
 });
 
+const CollectionPage = Loadable({
+    loader: () => import('../pages/PlansCollection/CollectionPage'),
+    loading: () => null
+});
+
 const RootRoutes = () => {
-    
-}
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Homepage} />
+                <Route path="/collections" exact component={CollectionPage} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default RootRoutes;
