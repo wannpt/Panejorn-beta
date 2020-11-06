4646828e348c6cf373344496d721b79b0597af41
setthawut leelawatthanapanit <setthawut.leela@gmail.com>
2020-11-06 14:52:16 +0700

    First time on GitKraken

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("Experiment Echo")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error{ 
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, Gopher!")
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }
