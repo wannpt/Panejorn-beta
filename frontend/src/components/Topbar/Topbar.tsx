98f17b916b9990b26e4767d8f3dbd29731eb28d4
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2020-11-14 22:42:54 +0700

    Add state to topbar

--- frontend/src/components/Topbar/Topbar.tsx
+++ frontend/src/components/Topbar/Topbar.tsx
@@ -4,25 +4,52 @@ import { useLocation } from 'react-router-dom';
 import '../../App.scss';
 import './Topbar.scss';
 
+
+
+type TopbarType = {
+    title: string,
+    path: string,
+    canReturn: boolean,
+    options?: React.FC[],
+    isEdit: boolean
+    styleClass: string,
+   // active: boolean
+}
+
+const topbarConstant: TopbarType[] = [
+    { title: 'หน้าแรก', path: '/' , canReturn: false, isEdit: false, styleClass: 'topbar'},
+    { title: 'แผนของฉัน', path: '/collections', canReturn: false, isEdit: false, styleClass: 'topbar gradient-background'},
+    { title: 'สำรวจ', path:'/explore',canReturn: false, isEdit: false, styleClass: 'topbar gradient-background'},
+    { title: 'ตั้งค่า', path:'/setting',canReturn: false, isEdit: false, styleClass: 'topbar gradient-background'},
+    ]
+
+
+
 function Topbar() {
     
-    let [topBarClass, setTopBarClass] =  useState('topbar')
+    let [topBar, setTopbar] =  useState<TopbarType>(topbarConstant[0])
     const location = useLocation();
 
     useEffect(() => {
         const path = location.pathname
-        const pattern = new RegExp('^/' + '$')
-        const selected = path.match(pattern);
-        if(selected)
-            setTopBarClass('topbar');
-        else
-            setTopBarClass('topbar gradient-background');
+        topbarConstant.map(el => {
+            const pattern = new RegExp('^' + el.path + '$')
+            const selected = path.match(pattern);
+            if(selected)
+                setTopbar(el)
+
+        })
+        // const pattern = new RegExp('^/' + '$')
+        // const selected = path.match(pattern);
+        // if(selected)
+        //     setTopbar();
+
 },[location.pathname]);
 
     
     return (
-        <div className={topBarClass}>
-            <p>หน้าแรก</p>
+        <div className={topBar.styleClass}>
+            <p>{topBar.title}</p>
         </div>
     );
 
--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Homepage.scss'
 
 function Homepage() {
     const screenWidth = window.innerWidth;
