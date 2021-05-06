805e6195293a2c04be2fb5e427d18a7f7343e60c
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2021-05-06 22:06:19 +0700

    Add bones on profile page

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -6,6 +6,7 @@ import { User, UserStats } from '../../constant/Types/UserTypes';
 import { UserConst, UserStatsConst } from '../../constant/constantVar/UserConst';
 import { Doughnut } from 'react-chartjs-2';
 import Loading from '../Loading/Loading';
+import ContentLoader from 'react-content-loader';
 
 const Profile = () => {
 	const history = useHistory();
@@ -51,7 +52,24 @@ const Profile = () => {
 		});
 	};
 	if (isLoading) {
-		return <Loading isLoading={isLoading} />;
+		return (
+			<ContentLoader
+				speed={1}
+				width={300}
+				height={667}
+				viewBox='0 0 400 170'
+				backgroundColor='#f3bb80'
+				foregroundColor='#eb8778'
+			>
+				<circle cx='248' cy='59' r='49' />
+				<circle cx='263' cy='66' r='8' />
+				<rect x='175' y='120' rx='0' ry='0' width='156' height='8' />
+				<rect x='204' y='137' rx='0' ry='0' width='100' height='8' />
+				<rect x='248' y='128' rx='0' ry='0' width='0' height='1' />
+				<rect x='247' y='126' rx='0' ry='0' width='1' height='8' />
+				<rect x='252' y='166' rx='0' ry='0' width='1' height='0' />
+			</ContentLoader>
+		);
 	}
 
 	if (!isLoading) {
