926167ce28c8aac3c81078efe84ad44066943372
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2021-05-06 23:48:17 +0700

    Add feature not ready on Explore page

--- frontend/src/pages/ExplorePage.tsx
+++ frontend/src/pages/ExplorePage.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import emotionSadLine from '@iconify-icons/ri/emotion-sad-line';
 
 function ExplorePage() {
-    return (
-        <div>
-            <p> Explore page works! </p>
-        </div>
-    )
+	return (
+		<div className='d-flex align-items-center justify-content-center' style={{ width: '100vw', height: '100vh' }}>
+			<div className=''>
+				<div className='d-flex justify-content-center'>
+					<Icon icon={emotionSadLine} color='#dbdbdb' width='120' height='120' />
+				</div>
+				<div className='d-block'>
+					<span style={{color:'#dbdbdb'}} className='big-title'>ขออภัย ฟีเจอร์นี้ยังไม่พร้อมใช้งาน</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExplorePage;
