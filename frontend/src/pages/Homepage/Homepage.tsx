2d72938579f224bf4a31f4d8852af71fe4945075
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2021-05-15 18:31:29 +0700

    change homepage card

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -4,6 +4,8 @@ import News from '../../components/News/News';
 import Logo from '../../Images/Group 23.svg';
 import { StatusCard } from '../../constant/Types/CardTypes';
 import Loading from '../../components/Loading/Loading';
+import Icon from '@iconify/react';
+import emotionSadLine from '@iconify-icons/ri/emotion-sad-line';
 
 const cardDefault: StatusCard = {
 	planName: 'เที่ยวหนึ่งวันกับคนอย่างเธอ',
@@ -20,10 +22,24 @@ function Homepage() {
 		<div>
 			{/* <Loading/> */}
 			<img src={Logo} className=' px-3 app-logo' alt='app logo' />
-			<div className='gradient-background default-padding'>
+			<div className='gradient-background default-padding '>
 				<div className='pb-4'>
 					<p className='small-title white-text mb-2'>แผนเที่ยวปัจจุบัน</p>
-					<Card data={cardDefault} isPinned={false} isStatus={true} />
+					<div className='card-container ' style={{paddingTop:"36px", paddingBottom:"36px"}}>
+						<div className='d-flex align-items-center justify-content-center'>
+							<div className=''>
+								<div className='d-flex justify-content-center'>
+									<Icon icon={emotionSadLine} color='#dbdbdb' width='120' height='120' />
+								</div>
+								<div className='d-block'>
+									<span style={{ color: '#dbdbdb' }} className='big-title'>
+										ขออภัย ฟีเจอร์นี้ยังไม่พร้อมใช้งาน
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* <Card data={cardDefault} isPinned={false} isStatus={true} /> */}
 				</div>
 			</div>
 			<div className='px-3 py-4'>
